d6710b9ded6daf82c20092b92054e2a84731ccb5
ejlepoudre <ejlepoudre@gmail.com>
2021-12-09 02:21:57 -0500

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,7 +4,7 @@ terraform {
     resource_group_name  = "terraform-rg"
     storage_account_name = "lobterraformstatestorejl"
     container_name       = "stateprod"
-    key                  = "dev.terraform.tfstate123"
+    key                  = "dev.terraform.tfstate1234"
   }
   
   required_providers {
